f1db94b8283bf560ab1d16d26c54908c8825bedd
MABODO Eltone <eltone.mabodo@wfp.org>
2024-08-23 15:09:37 +0700

    Created simple maps for reduced coping strategies index and the livelihoods coping strategies essential needs (crisis coping strategies)

--- code/MapsVisualizations.R
+++ code/MapsVisualizations.R
@@ -3,7 +3,72 @@
 library(tidyverse)
 library(sf)
 
-
+## Selected Provinces: Banteay Meanchey Kampong Cham Kampong Speu Kampot  Kratie Phnom Penh Preah Sihanouk  Siemreap Tboung Khmum  
 ## Source the Indicator Calculation File
 
 source("code/IndicatorCalculation.R")
+
+COData <- st_read("data/shape/khm_bnd_admin1_gov_wfp_ed2022.shp") %>% 
+  # Selecting the necessary columns
+  select(Adm1_Name, geometry) %>%
+  # Renaming the columns
+  rename(Province = Adm1_Name) %>% 
+  # Filtering the necessary provinces
+  filter(
+    Province == "Banteay Meanchey" | 
+      Province == "Kampong Cham" | 
+      Province == "Kampong Speu" | 
+      Province == "Kampot" | 
+      Province == "Kratie" | 
+      Province == "Phnom Penh" | 
+      Province == "Preah Sihanouk" | 
+      Province == "Siemreap" | 
+      Province == "Tboung Khmum") 
+
+###########################################################################################################
+# Reduced Coping Strategies Index
+
+# Merge with the Indicator Data
+RCSIndexMap <- COData %>% 
+    left_join(meanRCSIProvince, by = "Province")
+
+
+RCSIndexMap %>% 
+  ggplot() +
+  geom_sf(aes(fill = MeanRCSI), color = "black") +
+  facet_wrap(~Treatment) +
+  scale_fill_viridis_c() +
+  theme_minimal() +
+  labs(
+    title = "Mean Resilience Capacity Score Index by Province",
+    fill = "Mean RCSI"
+  ) +
+  theme(legend.position = "bottom")
+
+###########################################################################################################
+
+# Livelihoods Coping Strategies Essential Needs Index
+
+LSCENMap <- COData %>% 
+  left_join(SvyLCSENMaxProvince, by = "Province")
+
+LSCENMap %>%
+  ggplot() +
+  geom_sf(aes(fill = `Crisis coping strategies`), color = "black") +
+  facet_wrap(~Treatment) +
+  scale_fill_viridis_c() +
+  theme_minimal() +
+  labs(
+    title = "Max Livelihoods Coping Strategies Essential Needs Index by Province",
+    fill = "Max LCSEN"
+  ) +
+  theme(legend.position = "bottom")
+
+
+
+
+
+
+
+
+
