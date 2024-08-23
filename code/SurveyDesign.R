72c4a290bb1eceaaf34978f5d89f34db47060a5d
MABODO Eltone <eltone.mabodo@wfp.org>
2024-08-23 09:16:02 +0700

    Copleted setting the data for survey analysis

---
+++ code/SurveyDesign.R
@@ -0,0 +1,62 @@
+library(tidyverse)
+library(haven)
+library(labelled)
+library(survey)
+library(srvyr)
+
+
+
+# Read cover data
+SurveyDesignData<- read_dta("data/cover.dta")
+
+
+SurveyDesignData <- SurveyDesignData %>%
+  select(hhid, clusterid, strataid, GPS__Latitude, GPS__Longitude, GPS__Accuracy, GPS__Altitude, GPS__Timestamp, regiontype, poorscore) %>% 
+  rename(interview__key = hhid)
+
+SvyLCSENData <- LCSEN %>% 
+  left_join(SurveyDesignData, by = "interview__key") %>% 
+  #Set the data to survey designed data
+  as_survey_design(ids = clusterid, 
+                   strata = strataid, 
+                   weights = NULL)
+
+# Merge the FIES Data with the Survey Design Data
+SvyFIESData <- FIESData %>% 
+  left_join(SurveyDesignData, by = "interview__key") %>% 
+  # Set the data to survey designed data
+  as_survey_design(ids = clusterid, 
+                   strata = strataid, 
+                   weights = NULL)
+
+# Merge the rCSI Data with the Survey Design Data
+SvyrCSIData <- rCSIData %>% 
+  left_join(SurveyDesignData, by = "interview__key") %>% 
+  #Set the data to survey designed data
+  as_survey_design(ids = clusterid, 
+                   strata = strataid, 
+                   weights = NULL)
+
+
+# Merge the MAD Data with the Survey Design Data
+SvyMADData <- MADChildren %>% 
+  left_join(SurveyDesignData, by = "interview__key") %>% 
+  #Set the data to survey designed data
+  as_survey_design(ids = clusterid, 
+                   strata = strataid, 
+                   weights = NULL)
+
+# Merge the DietQuality data with the Survey Design Data
+SvyDietQualityData <- DietQuality %>% 
+  left_join(SurveyDesignData, by = "interview__key") %>% 
+  #Set the data to survey designed data
+  as_survey_design(ids = clusterid, 
+                   strata = strataid, 
+                   weights = NULL)
+
+## Complete setting the data for survey analysis
+
+############################################################################################################################################################
+
+
+
