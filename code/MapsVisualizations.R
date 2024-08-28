1e792fd0ff4dae63f11ea5f6543aaf2d6b16cf90
MABODO Eltone <eltone.mabodo@wfp.org>
2024-08-28 16:44:58 +0700

    The biggest change here was including the code to graph consumption based strategies and the table for who were the majority of household members reducing their meals sizes or proportions. There were also minor changes in the margins for the other graphs (LCS-EN, LSC-FS0 and color changes for the FIES graph

--- code/MapsVisualizations.R
+++ code/MapsVisualizations.R
@@ -5,6 +5,9 @@ library(sf)
 library(showtext)
 library(patchwork)
 library(cowplot)
+library(gridExtra)
+library(gt)
+library(grid)
 
 ## Selected Provinces: Banteay Meanchey Kampong Cham Kampong Speu Kampot  Kratie Phnom Penh Preah Sihanouk  Siemreap Tboung Khmum  
 ## Source the Indicator Calculation File
@@ -202,7 +205,7 @@ LCSENGraph <- ggplot(SvyLCSENMaxProvince, aes(x = Province, y = Pct_LCSENMax, fi
   theme_void() +
   theme(
     axis.text.x = element_text(family = "opensans", colour =  "#4B2E2A", size = 8, face = "bold",
-                               margin = margin(t = 0, b = 20)),
+                               margin = margin(t = -5, b = 20)),
     axis.text.y = element_blank(),
     axis.title = element_text(family = "opensans", size = 10, face = "bold"),
     axis.title.x = element_blank(),
@@ -219,7 +222,8 @@ LCSENGraph <- ggplot(SvyLCSENMaxProvince, aes(x = Province, y = Pct_LCSENMax, fi
     legend.key.size = unit(0.5, "cm"),  # Size of the legend keys
     legend.spacing.x = unit(0.2, "cm"),  # Horizontal spacing between legend items
     #legend.box.background = element_rect(color = "black", size = 0.5),  # Border around the legend box
-    legend.box.margin = margin(t = -10, b = 10))  # Margin around the legend box
+    legend.box.margin = margin(t = -10, b = 10))    # Margin around the legend box
+
 
 ## Combined Coping Srategies Indicator Graph
 CombinedCopingStrategiesGraph <- LCSFSGraph + LCSENGraph
@@ -259,7 +263,8 @@ ProvinceFIES <- read_xlsx("report tables/FIES.xlsx")
 
 
 FIESGraph <- ggplot(ProvinceFIES, aes(x = FIES, y = reorder(Province, FIES))) +  
-  geom_bar(stat = "identity", fill = "#0072B2", width = 0.7) +  # Set a blue fill color similar to the image and adjust bar width
+  geom_bar(stat = "identity", fill = "#8d1f17", width = 0.7) +  # Set a blue fill color similar to the image and adjust bar width
+  geom_vline(xintercept = 64.2, color = "black", size = 1) +  # Add a vertical line at x = 50
   scale_x_continuous(expand = c(0, 0),
                      limits = c(0,75),
                      breaks = seq(0, 75, 10),
@@ -276,23 +281,71 @@ FIESGraph <- ggplot(ProvinceFIES, aes(x = FIES, y = reorder(Province, FIES))) +
   ) +
   geom_text(aes(0,label = Province), 
             hjust = 0, size = 3.5, 
-            family = "opensans", color = "white",
+            family = "opensans", color = "#ffffff",
             nudge_x = 0.3) + # Add data labels
   geom_text(aes(x= 32, label = paste0(FIES, "%")), # Setting the x-axis position
             hjust = 0, size = 3, 
-            family = "opensans", color = "white") +  # Add data labels
+            family = "opensans", color = "#ffffff") +  # Add data labels
   labs(
     title = "Food Insecurity Experience Scale (FIES) by Province (%)",
-    subtitle = "Majority of households are food insecure in almost all provinces, except in Banteay Meanchey, where less than 50% \n of households are food insecure",
+    subtitle = "Majority of households are food insecure in almost all provinces, except in Banteay Meanchey, where less than 50% of households are food insecure",
     caption = "Data Source: Family Package Baseline"
   ) + 
   theme(plot.title = element_text(size = 12, family = "opensans", face = "bold"),
-        plot.subtitle = element_text(size = 10, family = "opensans", face = "italic", lineheight = 1.5),
+        plot.subtitle = element_text(size = 10, family = "opensans", face = "italic", lineheight = 1.5,
+                                     margin = margin(t = 10, b = 10)),
         plot.caption = element_text(size = 10, hjust = 0, family = "opensans", color = "black"))
 
 
+## Reduced Coping Strategies Index
+
+ReducedCopingStrategiesGraph <- ggplot(rCSICopingStrategies, aes(x = reorder(strategy, Percentage), y = Percentage)) +
+  geom_bar(stat = "identity", fill = "#254769", width = 0.6) +
+  geom_hline(yintercept = 0, color = "black", size = 1) +
+  geom_text(aes(label = paste0(Percentage, "%")), vjust = 10, size = 3.5, family = "opensans", color = "#ffffff") +
+  #coord_flip() + # Flips the coordinates for better readability
+  labs(title = "The most used consumption based was relying on less prefferd food",
+       subtitle = "Whenever reducing meal sizes or portions was adopted, women household members are making a big sacrifice compared to men",
+       x = "Coping Strategies Adopted",
+       y = "Percentage",
+       caption = "Data Source: Family Package Baseline") +
+  theme_void() +
+  theme(
+    # Theme to the panel
+    panel.background = element_rect(fill = "white", color = "white", size = 0.5),
+    panel.grid.major.y = element_line(color = "grey", size = 0.5),
+    # Theme the x- axis
+    axis.text.x = element_text(lineheight = 1.8, family = "opensans", size = 8, color = "black", face = "bold", margin = margin(t = -10)),
+    axis.title.x = element_text(family = "opensans", size = 10, color = "black", face = "bold", margin = margin(b = 10, t = 10)),
+    axis.ticks.x  = element_blank(),
+    # Theme the titles
+    plot.title = element_text(family = "opensans", size = 12, color = "black", face = "bold", margin = margin(b = 10, t = 10, l = 20)),
+    plot.subtitle = element_text(family = "opensans", size = 10, color = "black", face = "italic", margin = margin(b = 10, l = 20)),
+    plot.caption = element_text(family = "opensans", size = 10, color = "black", margin = margin(t = 10, l = 20, b = 10), hjust = 0))
 
 
-
+StrategiesTable <- rCSIMealSizeWhoTable %>% 
+  gt() %>%
+  tab_header(
+    title = "Who is reducing Meal Sizes (%)") %>% 
+  fmt_number(
+    columns = c(`Male Adults`, `Female Adults`, `Other Options`),
+    decimals = 1
+  ) %>% 
+  tab_options(
+    heading.align = "left",
+    #table.font.sizes = 10,
+    table.align = "center",
+    data_row.padding = px(5)) %>% 
+  tab_style(
+    style = cell_text(
+      weight = "bold",
+      color = "black",
+      align = "center",
+      font = "opensans"
+    ),
+    locations = cells_column_labels(
+     everything()))
+  
 
 
